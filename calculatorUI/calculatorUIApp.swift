c8d5b5bbc12cbf280487674db251948969d8d042
Kamil <kamilwojcickinjr@gmail.com>
2023-02-04 19:53:15 +0100

    Initial Commit

---
+++ calculatorUI/calculatorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorUIApp.swift
+//  calculatorUI
+//
+//  Created by Kamil on 04/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct calculatorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
